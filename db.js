ed0194ecbbc43971aeff37b434f35ed515f43449
Ronihe <herongrong2011@gmail.com>
2018-12-23 22:53:30 -0800

    server/.js done

--- db.js
+++ db.js
@@ -1,3 +1,11 @@
 /** Database setup for goals. */
 
 const { Client } = require('pg');
+const { DB_URI } = require('./config');
+
+const client = new Client({
+  connectionString: DB_URI
+});
+
+client.connect();
+module.exports = client;
